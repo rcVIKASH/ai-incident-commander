3c3fdfda30526171c49f95db7f8c3ef404675e66
rcVIKASH <vikash1610m@gmail.com>
2026-08-14 17:36:20 +0530

    feat: add ChatOpenRouter model and update dependencies for improved functionality

--- app/langGraph/llm.ts
+++ app/langGraph/llm.ts
@@ -1,4 +1,5 @@
 import { ChatGroq } from "@langchain/groq";
+import { ChatOpenRouter } from "@langchain/openrouter";
 
 export const model1 = new ChatGroq({
   model: "openai/gpt-oss-120b",
@@ -13,4 +14,9 @@ export const model2 = new ChatGroq({
 export const model3 = new ChatGroq({
   model: "qwen/qwen3.6-27b",
   apiKey: process.env.GROQ_API_KEY,
+});
+
+export const model4 = new ChatOpenRouter({
+  model: "openai/gpt-5.6-luna",
+  apiKey: process.env.OPENROUTER_API_KEY,
 });
